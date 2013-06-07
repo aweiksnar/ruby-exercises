b70c83d87de4201ccdc7f7110b2f2a517263cd30
aweiksnar <aweiksnar@gmail.com>
2013-06-07 11:24:31 -0500

    Update project_euler_1.rb
    Change to class-level methods

--- project_euler_1.rb
+++ project_euler_1.rb
@@ -1,31 +1,29 @@
 class Sum_of_multiples
 
-  def initialize(range)
+  def self.test(range)
     @num_array = []
     scan(range)
   end
 
-  def scan(range)
+  def self.scan(range)
     (range).each do |x|
       check_nums(x)
     end
     sum_arrray
   end
 
-  def check_nums(x)
+  def self.check_nums(x)
     if (x % 5) == 0 && (x % 3) == 0
       @num_array << x
-    elsif (x % 5) == 0
-      @num_array << x
-    elsif (x % 3) == 0
+    elsif (x % 5) == 0 || (x % 3) == 0
       @num_array << x
     end
   end
 
-  def sum_arrray
+  def self.sum_arrray
     p @num_array.inject {|sum, x| sum + x}
   end
 
 end
 
-Sum_of_multiples.new(1..1000) #=> 234168
+Sum_of_multiples.test(1..1000) #=> 234168
