a3a66b7ae9bf8749bf5d813a2cd7fc33e26f1748
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-08-22 09:34:02 -0500

    options to settings

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -9,22 +9,22 @@ set :secret_number, rand(100)
 get "/" do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => options.secret_number, :message => message}
+  erb :index, :locals => {:number => settings.secret_number, :message => message}
 end
 
 def check_guess(guess)
   if guess.is_a? String
     guess = guess.to_i
-    if guess - 5 > options.secret_number
+    if guess - 5 > settings.secret_number
       "Way Too High"
-    elsif guess > options.secret_number
+    elsif guess > settings.secret_number
       "Too high"
-    elsif guess + 5 < options.secret_number
+    elsif guess + 5 < settings.secret_number
       "Way too low"
-    elsif guess < options.secret_number
+    elsif guess < settings.secret_number
       "Too Low"
     else
-      "You got it! The secret number is #{options.secret_number}"
+      "You got it! The secret number is #{settings.secret_number}"
     end
   else
     "Try and guess the secret number"
