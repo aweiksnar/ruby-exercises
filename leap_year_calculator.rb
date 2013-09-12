9fb62adc4c72d66d2acd1c066428e42311e04162
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-12 16:07:16 -0500

    refactor leap? method in leap year calculator

--- leap_year_calculator.rb
+++ leap_year_calculator.rb
@@ -2,28 +2,28 @@ class Year
   def initialize(year)
     @year = year
   end
- 
+
   def divisible_by_4?
     (@year % 4) == 0 ? true : false
   end
- 
+
   def divisible_by_100?
     (@year % 100) == 0 ? true : false
   end
- 
+
   def divisible_by_400?
     (@year % 400) == 0 ? true : false
   end
- 
+
   def leap?
-    if divisible_by_4? == true && divisible_by_400? == true
-      return true
-    elsif divisible_by_4? == true && divisible_by_100? == false
-      return true
+    if divisible_by_4? && divisible_by_400?
+      true
+    elsif divisible_by_4? && divisible_by_100?
+      true
     else
-      return false
+      false
     end
   end
 end
- 
-Year.new(2000).leap? #=> true
+
+puts Year.new(2000).leap? #=> true
