0d8ea392c3fa91282e8c26cd8c5ac23222848b98
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-14 11:46:34 -0500

    remove tertiary operators from leap year calc

--- leap_year_calculator.rb
+++ leap_year_calculator.rb
@@ -4,15 +4,15 @@ class Year
   end
 
   def divisible_by_4?
-    (@year % 4) == 0 ? true : false
+    @year % 4 == 0
   end
 
   def divisible_by_100?
-    (@year % 100) == 0 ? true : false
+    @year % 100 == 0
   end
 
   def divisible_by_400?
-    (@year % 400) == 0 ? true : false
+    @year % 400 == 0
   end
 
   def leap?
